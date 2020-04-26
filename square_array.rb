d5943e7bd5d719cd6585422f8b26e2df6873f553
idith almog <almog.idith@gmail.com>
2020-04-26 22:12:21 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,9 @@
 def square_array(array)
-  new_array=""
+  new_array=[""]
   array.each do |number|
   square=number**2
   puts "#{square}"
-
-  new_array << square
+  new_array.push(square)
   end
   return new_array
 end
